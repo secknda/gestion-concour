7002fbed32d36d223edb5feb4e8217cd9bf15c19
Babacar Ndong <ndongbabacar100@gmail.com>
2021-11-12 22:18:07 +0000

    Design the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VerticalNavComponent } from './vertical-nav/vertical-nav.component';
+import { HorizontalNavComponent } from './horizontal-nav/horizontal-nav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VerticalNavComponent,
+    HorizontalNavComponent,
+    DashboardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/horizontal-nav/horizontal-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-horizontal-nav',
+  templateUrl: './horizontal-nav.component.html',
+  styleUrls: ['./horizontal-nav.component.css']
+})
+export class HorizontalNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
