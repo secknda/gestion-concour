029815be9eec8ae4214f10540d2ae7c80a3a99a5
Babacar Ndong <ndongbabacar100@gmail.com>
2022-01-20 13:56:46 +0000

    Change URL DB

--- src/service/networking.service.ts
+++ src/service/networking.service.ts
@@ -7,7 +7,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
 })
 export class NetworkingService {
 
-  readonly BASE_URL = "http://54.89.219.231:5454/";
+  readonly BASE_URL = "http://3.84.42.232:5454/";
 
   private subject = new BehaviorSubject<any>("");
   adminData: any;
